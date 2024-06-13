58feb50bf8aee9a64ceead155ae2b8edac386972
jdeastin <j.d.east@icloud.com>
2024-06-13 14:40:08 -0700

    Revert "styling up frontend, adding dummy data, working on avatar."
    This reverts commit a30e54f1c61d228a0357c12c1b08047cef1fd798.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,18 @@
 import "./App.css";
-import { useEffect } from "react";
+import Education from "./components/Education";
 import Header from "./components/Header";
+import Nav from "./components/Nav";
 import Portfolio from "./components/Portfolio";
-import Footer from "./components/Footer";
 
 function App() {
-  useEffect(() => {
-    document.body.classList.add("flex");
-    document.body.classList.add("h-full");
-    document.body.classList.add("bg-zinc-50");
-    document.body.classList.add("dark:bg-black");
-
-    const flex = document.getElementById("flex");
-    flex.classList.add("flex");
-    flex.classList.add("w-full");
-
-    const flexInset = document.getElementById("flexInset");
-    flexInset.classList.add("fixed");
-    flexInset.classList.add("inset-0");
-    flexInset.classList.add("flex");
-    flexInset.classList.add("justify-center");
-    flexInset.classList.add("sm:px-8");
-
-    const root = document.getElementById("root");
-    root.classList.add("relative");
-    root.classList.add("flex");
-    root.classList.add("w-full");
-    root.classList.add("flex-col");
-  }, []);
-
   return (
     <>
+      <Nav />
       <Header />
+      <Education />
       <Portfolio />
-      <Footer />
     </>
-  );
+  )
 }
 
 export default App;
